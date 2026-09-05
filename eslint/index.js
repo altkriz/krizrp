40a6221eb3e21f61a6995bbbaa6097a68fa7c544
Kashif Raza <30174244+altkriz@users.noreply.github.com>
2026-09-05 16:09:55 +0530

    chore: refactor project metadata and CI
    Update issue templates, clean up unused configuration files, and establish a new Android CI workflow.

--- eslint/index.js
+++
@@ -1,8 +0,0 @@
-/* eslint-disable object-shorthand */
-// eslint-plugin-react-native-stylesheet/index.js
-
-module.exports = {
-    rules: {
-        'enforce-spacing-values': require('./eslint-plugin-enforce-spacing-values'),
-    },
-}
